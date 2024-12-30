848cac011a9d47dff0c7700f0cb3cd1a244f2b84
ankit <sahuankit.murhu@gmail.com>
2024-12-31 01:04:25 +0530

    Praiser done

--- server/src/ai/ai.ts
+++ server/src/ai/ai.ts
@@ -9,6 +9,7 @@ const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 const basePrompt = {
     roastPrompt: "Roast this instgram user in 8 - 12 lines at max.",
+    praisePrompt: "Praise this instgram user in 8 - 12 lines at max."
 }
 
 export async function roaster(id: string){
@@ -29,3 +30,20 @@ export async function roaster(id: string){
     }
 }
 
+export async function praiser(id: string) {
+    try {
+        const user = await getInstaDetails(id);
+        const prompt = basePrompt.roastPrompt +
+                `A instagram user with the name ${user.username}
+                and instagram id ${id} has their account with
+                ${user.followers} followers and ${user.following}
+                followings and they've posted ${user.posts} posts.
+                ${user.bio != '' ? `Their bio says ${user.bio}` : " "}`
+
+        const response = await model.generateContent(prompt);
+        return response.response.text();
+    } catch (error) {
+        console.error(error);
+        return "An error occurred while generating praise.";
+    }
+}
