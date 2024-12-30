f7ac02b8ff58cff1d95e4df73e1fbbe0a3200560
ankit <sahuankit.murhu@gmail.com>
2024-12-31 01:05:11 +0530

    Roast route done

--- server/src/app/main.ts
+++ server/src/app/main.ts
@@ -1,8 +1,20 @@
 import express from 'express';
+import { praiser, roaster } from '../ai/ai';
 
 const PORT = 3000;
 const app = express();
 
+app.get('/api/roast', async (req, res) => {
+    const id = "kmr_ankitt";
+    try {
+        const roast = await roaster(id);
+        console.log(roast);
+        res.send(roast);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("An error occurred while fetching Instagram details.");
+    }
+});
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
