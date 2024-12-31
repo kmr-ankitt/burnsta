d6e8c11768d71e45b34a0a31504bb4c7c7d8cc00
ankit <sahuankit.murhu@gmail.com>
2024-12-31 22:34:11 +0530

    Dynamic moving gradient bg

--- www/src/app/page.tsx
+++ www/src/app/page.tsx
@@ -1,7 +1,13 @@
+import Hero from "@/components/Hero";
+import { BackgroundGradientAnimation } from "@/components/ui/background-gradient-animation";
+
 export default function Home() {
   return (
-    <div>
-      home
+    <div className="h-screen w-full">
+
+      <BackgroundGradientAnimation  className="z-10 fixed h-full w-full ">
+        <Hero />
+      </BackgroundGradientAnimation>
     </div>
   );
 }
