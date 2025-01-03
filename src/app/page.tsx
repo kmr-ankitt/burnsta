99f18a979b2deb2b0c38d3417ad7e83d25a82d1d
ankit <sahuankit.murhu@gmail.com>
2025-01-03 08:02:23 +0530

    universal bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
 import Hero from "@/components/Hero";
-import { BackgroundGradientAnimation } from "@/components/ui/background-gradient-animation";
 
 export default function Home() {
   return (
     <div className="h-screen w-full">
-
-      <BackgroundGradientAnimation  className="z-10 fixed h-full w-full ">
         <Hero />
-      </BackgroundGradientAnimation>
     </div>
   );
 }
