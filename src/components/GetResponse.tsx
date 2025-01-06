e577c5db95e9386f029de3e1430bc369b68567e3
ankit <sahuankit.murhu@gmail.com>
2025-01-06 20:10:09 +0530

    Local host fixed

--- src/components/GetResponse.tsx
+++ src/components/GetResponse.tsx
@@ -16,7 +16,7 @@ export default function GetResponse({
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(`http://localhost:3000/api/${type}`, {
+        const response = await fetch(`/api/${type}`, {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
